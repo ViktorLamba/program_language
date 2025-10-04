89ca2eb67a9d853530595ea8efd19e665b17cb56
ViktorLamba <viktorlamba05@gmail.com>
2025-10-04 14:14:59 +0300

    Задание 3

--- lab3.py
+++ lab3.py
@@ -5,3 +5,8 @@ print(f"Квадраты чисел от 1 до 10: {list_1}")
 #Задание 2
 list_2 = [x for x in range(1,20) if x % 2 == 0]
 print(f"Чётные числа от 1 до 20: {list_2}")
+
+#Задание 3
+words = ["python", "Java", "c++", "Rust", "go"]
+list_3 = [_.upper() for _ in words if len(_) > 3]
+print(f"Отфильтрованные слова: {list_3}")
