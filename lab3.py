b70954edf81923c9f94958a5d41aa2030f8e2176
ViktorLamba <viktorlamba05@gmail.com>
2025-10-04 14:17:32 +0300

    Задание 10

--- lab3.py
+++ lab3.py
@@ -142,3 +142,28 @@ def calculate_birthday_info():
 
 # Запуск расчета
 calculate_birthday_info()
+
+
+
+#Задание 10
+from datetime import datetime
+
+def format_datetime_russian(dt):
+    # Русские названия месяцев
+    months = {
+        1: "января", 2: "февраля", 3: "марта", 4: "апреля",
+        5: "мая", 6: "июня", 7: "июля", 8: "августа",
+        9: "сентября", 10: "октября", 11: "ноября", 12: "декабря"
+    }
+    
+    day = dt.day
+    month = months[dt.month]
+    year = dt.year
+    time = dt.strftime("%H:%M")
+    
+    return f"Сегодня {day} {month} {year} года, время: {time}"
+
+# Пример использования
+current_time = datetime.now()
+formatted_string = format_datetime_russian(current_time)
+print(formatted_string) 
