039a122bfc96d65cfd63530cc142a5a3617eb9f2
ViktorLamba <viktorlamba05@gmail.com>
2025-10-04 14:15:21 +0300

    Задание 4

--- lab3.py
+++ lab3.py
@@ -10,3 +10,25 @@ print(f"Чётные числа от 1 до 20: {list_2}")
 words = ["python", "Java", "c++", "Rust", "go"]
 list_3 = [_.upper() for _ in words if len(_) > 3]
 print(f"Отфильтрованные слова: {list_3}")
+
+#Задание 4
+class Countdown:
+    def __init__(self, n):
+        self.n = n
+        self.current = n
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.current < 1:
+            raise StopIteration
+        value = self.current
+        self.current -= 1
+        return value
+
+# Пример использования
+print("Countdown от 5:")
+for x in Countdown(5):
+    print(x, end=" ")
+print()
