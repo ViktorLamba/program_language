f499cf3e2b4972a1a927705193d71320c1d1374a
ViktorLamba <viktorlamba05@gmail.com>
2025-10-08 16:02:54 +0300

    тест пичарма

--- lab3.py
+++ lab3.py
@@ -1,6 +1,6 @@
 #Задание 1
-# list_1 = [x ** 2 for x in range(1,11)]
-# print(f"Квадраты чисел от 1 до 10: {list_1}")
+list_1 = [x ** 2 for x in range(1,11)]
+print(f"Квадраты чисел от 1 до 10: {list_1}")
 
 #Задание 2
 # list_2 = [x for x in range(1,20) if x % 2 == 0]
